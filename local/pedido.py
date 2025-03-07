b2963e3d679cbecbc8e9edb4fc3f6dbf23bbfe87
zUlisesz <zperplexz@gmail.com>
2025-03-07 11:00:18 -0600

    Update pedido.py

--- local/pedido.py
+++ local/pedido.py
@@ -33,61 +33,8 @@ class Pedido:
     
         return f'{cadenaNormal}\n\n{cadenaMixto}\n\n{cadenaTinga}\n\n{cadenaPata}\n\n{cadenaBebidaMedio}\n\n{coffe}\n\nTotal: ${Pedido.calcularCarrito(lista)}'
     
-    @staticmethod
-    def sendPackage(carrito):
-        key = Connection.connectBD()
-        
-        if key is None:
-            print("No se pudo establecer la conexión con la base de datos.")
-            return
-        
-        cursor = key.cursor()
-        
-        query = '''
-        INSERT INTO local.pedido (molote_normal, molote_mixto, tostada_tinga, tostada_pata, refresco, cafe, fecha, cuenta_total)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s);\ncommit; '''
-        
-        values = (
-            carrito[0].cantidad,
-            carrito[1].cantidad,
-            carrito[2].cantidad,
-            carrito[3].cantidad,
-            carrito[4].cantidad,
-            carrito[5].cantidad,
-            date.today(),
-            Pedido.calcularCarrito(carrito),
-        )
-        
-        try:
-            cursor.execute(query, values)
-            print("Pedido enviado correctamente.")
-        except mysql.connector.Error as error:
-            print('Error: {}'.format(error))
-        finally:
-            cursor.close()
-            key.close()
+    
             
-    @staticmethod
-    def getAll():
-        key = Connection.connectBD()
-        
-        if key is None:
-            print("No se pudo establecer la conexión con la base de datos.")
-            return
-        
-        cursor = key.cursor()
-        
-        try:
-            cursor.execute("SELECT * FROM pedido")
-            myresult = cursor.fetchall()
-            
-            for x in myresult:
-                print(x)
-        except mysql.connector.Error as error:
-            print('Error: {}'.format(error))
-        finally:
-            cursor.close()
-            key.close()
 
     @staticmethod
     def getImportantInfo():
