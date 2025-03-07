5543fa3f095e54a2e35e27df99ab24c0d4e43c1d
zUlisesz <zperplexz@gmail.com>
2025-03-07 11:00:56 -0600

    Update producto.py

--- local/producto.py
+++ local/producto.py
@@ -1,4 +1,3 @@
-import json
 from pedido import Pedido
 
 class Producto:
@@ -22,35 +21,4 @@ class Producto:
         
     def resetCantidad(self):
         self.cantidad = 0
-        
-    @staticmethod     
-    def guardarPedido(filename):
-        try:
-            with open(filename, "r", encoding="utf-8") as file:
-                existing_data = json.load(file)
-        except FileNotFoundError:
-            existing_data = []
-
-        newCarrito = []
-        for producto in Producto.carrito:
-            data = {
-                'Tipo': producto.tipo,
-                'Cantidad': producto.cantidad,
-                'Precio': producto.precio
-            }
-            newCarrito.append(data)
-
-        existing_data.extend(newCarrito)
-        
-        try:
-            with open(filename, 'w', encoding='utf-8') as file:
-                json.dump(existing_data, file, indent=4, ensure_ascii=False)
-            print("Datos enviados correctamente a: {}".format(filename))
-        except Exception as e:
-            print("{}".format(e))
     
-    @classmethod
-    def savePedido(cls, archivo):
-        pedidos = [{"Tipo": u.tipo, "Precio": u.precio, "Cantidad": u.cantidad} for u in cls.carrito]
-        with open(archivo, "w") as f:
-            json.dump(pedidos, f, indent=4)
