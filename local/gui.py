c300957af2e5ae32841f26461b06b4e41e4e867d
zUlisesz <zperplexz@gmail.com>
2025-03-06 23:54:37 -0600

    Update gui.py

--- local/gui.py
+++ local/gui.py
@@ -1,43 +1,63 @@
 from tkinter import *
 from producto import Producto
 from pedido import Pedido
+from PIL import Image, ImageTk
 
-def main(): 
-        
-    global mainWindow
-    global frame
-    global carritoLabel
+def moverPedidos():
+    mainWindow.withdraw()
+    ventanaPedido()
     
-    #ventana principal
+
+def moverVentas():
+    pass
+
+def main():
+    global mainWindow
     mainWindow = Tk()
-    mainWindow.title('Main')
-    mainWindow.config(bg = "AliceBlue")
-    mainWindow.resizable(width= False , height= False)
+    mainWindow.geometry('400x400')
+    
+    pedidos = Button( mainWindow, text = 'Hacer Pedido',font = ('Aptos', 20, 'bold'),  command= moverPedidos)
+    ventas = Button(mainWindow, text = 'Revisar ventas', font = ('Aptos', 20 , 'bold'), command= moverVentas)
+    
+    pedidos.pack(pady = 20 )
+    ventas.pack(pady = 20)
+    
+    mainWindow.mainloop()
+    
+    
+def ventanaPedido(): 
+        
+    global ventasWindow, carritoLabel
+    
+    ventasWindow = Toplevel()
+    ventasWindow.title('Main')
+    ventasWindow.config(bg = "AliceBlue")
+    ventasWindow.resizable(width= False , height= False)
     
     #frame para imprimir la cuenta
-    frame = LabelFrame(mainWindow, text = 'CUENTA TOTAL', padx = 70, pady = 80, border= 4,)
+    frame = LabelFrame(ventasWindow, text = 'CUENTA TOTAL', padx = 70, pady = 80, border= 4,)
     carritoLabel = Label(frame, text= Pedido.elementosCarrito(carrito), font= ('bold') )
     carritoLabel.pack()
     
     #letreros 
-    mainLabel = Label(mainWindow, text = 'CREANDO PEDIDO', font= ('Aptos', 26, 'bold'), bg = 'AliceBlue')
-    molotesLabel = Label(mainWindow , text = 'MOLOTES', font= ( 'Aptos', 18,'bold'), bg = 'AliceBlue')
-    tostadasLabel = Label(mainWindow, text= 'TOSTADAS',font= ('Aptos', 18,'bold'), bg = 'AliceBlue')
-    bebidasLabel = Label(mainWindow, text = 'BEBIDAS', font= ('Aptos', 18,'bold'), bg = 'AliceBlue')
+    mainLabel = Label(ventasWindow, text = 'CREANDO PEDIDO', font= ('Aptos', 26, 'bold'), bg = 'AliceBlue')
+    molotesLabel = Label(ventasWindow , text = 'MOLOTES', font= ( 'Aptos', 18,'bold'), bg = 'AliceBlue')
+    tostadasLabel = Label(ventasWindow, text= 'TOSTADAS',font= ('Aptos', 18,'bold'), bg = 'AliceBlue')
+    bebidasLabel = Label(ventasWindow, text = 'BEBIDAS', font= ('Aptos', 18,'bold'), bg = 'AliceBlue')
     
     #botones para agregar molotes
-    normalBoton = Button(mainWindow, text = 'NORMAL', font= ('Aptos', 14,'bold'), command= lambda: updateCar(moloteNormal),border= 4)
-    mixtoBoton = Button(mainWindow, text = 'MIXTO', font= ('Aptos', 14,'bold'), command= lambda: updateCar(moloteMixto), border= 4)
+    normalBoton = Button(ventasWindow, text = 'NORMAL', font= ('Aptos', 14,'bold'), command= lambda: updateCar(moloteNormal),border= 4)
+    mixtoBoton = Button(ventasWindow, text = 'MIXTO', font= ('Aptos', 14,'bold'), command= lambda: updateCar(moloteMixto), border= 4)
     
     #botones para agregar tostadas
-    tingaBoton = Button(mainWindow, text = 'TINGA', font= ('Aptos', 14,'bold'), command= lambda: updateCar(tostadaTinga), border= 4)
-    pataBoton  = Button(mainWindow, text = 'PATA', font= ('Aptos', 14,'bold'), command= lambda: updateCar(tostadaPata), border = 4)
+    tingaBoton = Button(ventasWindow, text = 'TINGA', font= ('Aptos', 14,'bold'), command= lambda: updateCar(tostadaTinga), border= 4)
+    pataBoton  = Button(ventasWindow, text = 'PATA', font= ('Aptos', 14,'bold'), command= lambda: updateCar(tostadaPata), border = 4)
     
     #boton para agregar bebidas
-    refresco = Button(mainWindow, text  = 'REFRESCO', font= ('Aptos', 14,'bold'), command= lambda: updateCar(bebidaMedio), border = 4)
-    cafe = Button(mainWindow, text  = 'CAFÉ', font= ('Aptos', 14,'bold'), command= lambda: updateCar(coffee), border = 4)
+    refresco = Button(ventasWindow, text  = 'REFRESCO', font= ('Aptos', 14,'bold'), command= lambda: updateCar(bebidaMedio), border = 4)
+    cafe = Button(ventasWindow, text  = 'CAFÉ', font= ('Aptos', 14,'bold'), command= lambda: updateCar(coffee), border = 4)
     
-    guardarBoton =  Button(mainWindow, text  = 'GUARDAR', font= ('Aptos', 16, 'bold'), padx = 14 , border = 4,  command= save)
+    guardarBoton =  Button(ventasWindow, text  = 'GUARDAR', font= ('Aptos', 16, 'bold'), padx = 14 , border = 4,  command= save)
 
     #empaquetado de los widgets de la pantalla
     mainLabel.grid(row  = 0 , column= 1, columnspan= 3, pady = 10 ,sticky= 'W')
@@ -58,7 +78,7 @@ def main():
     
     guardarBoton.grid(row = 7 , column= 0, pady = 20,sticky= 'E')
 
-    mainWindow.mainloop()
+    ventasWindow.mainloop()
     
 
 def save():
